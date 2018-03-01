af117464f34e292c4733da5093665d18e52d2c26
cashcs88 <artur2005cash@yandex.ru>
2018-03-01 22:03:48 +0200

    toggle plugin option added to general wp settings page

--- wp-share-provider/wp-share-provider.php
+++ wp-share-provider/wp-share-provider.php
@@ -4,6 +4,14 @@
  *
  */
 
+
+defined('ABSPATH') || exit;
+
+if (!defined('SHARE_PROVIDER_PLUGIN_DIR')) {
+    define('SHARE_PROVIDER_PLUGIN_DIR', __DIR__);
+    define('SHARE_PROVIDER_PLUGIN_VERSION', '1.0.0');
+}
+
 class ShareProvider
 {
     private static $instance = null;
@@ -25,9 +33,31 @@ class ShareProvider
 
     private function __construct()
     {
+        add_action('wp_enqueue_scripts', array($this, 'share_provider_enqueue')); //Enueue assets for front-end
+
         add_action('admin_menu', array($this, 'initShareMenu')); //Add Plugin Setting Page to menu
-        add_action('admin_init', array($this, 'initShareSettings')); //Register plugin Settings
         add_action('admin_init', array($this, 'initShareSettingsPage')); //Register plugin Settings
+
+        add_filter('the_content', array($this, 'showShareProvider')); //Show plugin on front
+    }
+
+    public function share_provider_enqueue()
+    {
+        wp_register_style(
+            'share-provider-styles',
+            SHARE_PROVIDER_PLUGIN_DIR . '/assets/css/styles.css',
+            array(),
+            SHARE_PROVIDER_PLUGIN_VERSION
+        );
+
+        wp_register_script('share-provider-scrips',
+            SHARE_PROVIDER_PLUGIN_DIR . '/assets/js/main.css', array('jquery'),
+            SHARE_PROVIDER_PLUGIN_VERSION,
+            true
+        );
+
+        wp_enqueue_style('share-provider-styles');
+        wp_enqueue_script('share-provider-scrips');
     }
 
     public function initShareMenu()
@@ -66,28 +96,53 @@ class ShareProvider
         );
     }
 
-    public function initShareSettings()
-    {
-
-    }
-
     public function initShareSettingsPage()
     {
-        /*
         add_settings_section(
-            'global',
-            'Global Share Plugin settings',
-            'initGlobalSharePluginSettingsSection',
-            'share-provider-plugin' );
-        */
+            'enable-disable-share-provider-plugin',
+            'Enable plugin',
+            array($this, 'general_settings_callback'),
+            'general'
+        );
+        add_settings_field(
+            'toggle_share_provider',
+            'Share plugin Enabled',
+            array($this, 'enable_disable_callback'),
+            'general',
+            'enable-disable-share-provider-plugin',
+            array('Enable this option to Enable Share Provider plugin')
+        );
+        register_setting(
+            'general',
+            'toggle_share_provider'
+        );
     }
 
-    public function initGlobalSharePluginSettingsSection() {
+    public function general_settings_callback()
+    {
+        echo 'This option provide you enable or disable current plugin. Even if plugin is active, you have possibility to disable all plugin functions using this option.';
+    }
 
+    public function enable_disable_callback($args)
+    {
+        $html = '<input type="checkbox" id="toggle_share_provider" name="toggle_share_provider" value="1"' .
+            checked(1, get_option('toggle_share_provider'), false) . '/>';
+        $html .= '<label for="toggle_share_provider">' . $args[0] . '</label>';
+        echo $html;
+    }
+
+    public function showShareProvider($content)
+    {
+        if (get_option('toggle_share_provider') && is_single()) {
+            $content .= '<div class="">Share</div>';
+        }
+        return $content;
     }
 }
 
 ShareProvider::getInstance();  // Getting class object
+
+/** start NOTES **/
 
 //All is the same - Getting class object
 //
